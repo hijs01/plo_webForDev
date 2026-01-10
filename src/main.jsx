460bd37f23294de44ea2cf883146f4e45e650557
unknown <hijs0146@gmail.com>
2026-01-09 20:49:50 -0500

    라우터 수정

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
 
-    <BrowserRouter basename="/plo_webForDev">
+    <BrowserRouter basename="/CUCO_website">
       <App />
     </BrowserRouter>
 );
